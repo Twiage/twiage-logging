65ce9c5a6dbb8549fab0993d54a07ac956d9f4a6
Ilya Eremin <IlyaEremin@users.noreply.github.com>
2022-02-07 16:26:34 +0300

    fixing critical vulnerabilities of yarn audit

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-'use strict';
-
-const GlobalRequestLogger = require('./lib/globalRequestLogger');
 const logger = require('./lib/logger');
 const Middleware = require('./lib/middleware');
 
 module.exports = {
-  GlobalRequestLogger,
   logger,
   Middleware
 };
